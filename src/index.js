004183eaa6d2f6b49e4f6ee382a2c82437244fff
Sebas-AuxWeb <auxweb1@outlook.com>
2022-09-28 17:06:40 -0500

    SocketIO #2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-const { info } = require("console");
 const express = require("express")
 const { createServer } = require("http");
 const path = require("path");
@@ -14,28 +13,32 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/a", (req, res) => {
-    let dat = console.log("yes")
+// app.get("/a", (req, res) => {
+//     let dat = console.log("yes")
 
-    res = dat
-});
+//     res = dat
+// });
+
+// io.on("connection", socket => {
+
+//     // console.log("Client Connected: ", io.engine.clientsCount)
+//     console.log("Client Socket ID", socket.id);
+
+//     // socket.on("disconnect", () => {
+//     //     console.log("Client: ", socket.id ,"Disconnect")
+//     // })
+
+//     socket.conn.once("upgrade", () => {
+//         console.log("HTTP Long-Polling Move To: ", socket.conn.transport.name);
+//     })
+
+// });
+
+
+/* ------------------------------------------- ENVIANDO EVENTOS ----------------------------------------------------------- */
 
 io.on("connection", socket => {
-
-    // console.log("Client Connected: ", io.engine.clientsCount)
-    console.log("Client Socket ID", socket.id);
-
-    // socket.on("disconnect", () => {
-    //     console.log("Client: ", socket.id ,"Disconnect")
-    // })
-
-    socket.conn.once("upgrade", () => {
-        console.log("HTTP Long-Polling Move To: ", socket.conn.transport.name);
-    })
-
-});
-
-
-
+    socket.emit("Welcome", "ConnectionDone Yep");
+})
 
 httpServer.listen(10000);
--- src/views/js/index.js
+++ src/views/js/index.js
@@ -1,31 +1,40 @@
 const socket = io();
 
-const checkSocketStatus = () => {
-    console.log("Estatus: ", socket.connected);
-}
+/* EVENTOS BASICOS DEL LADO DEL CLIENTE */
 
-socket.on("connect", () => {
-    console.log("Se conecto: ", socket.id);
-});
+// const checkSocketStatus = () => {
+//     console.log("Estatus: ", socket.connected);
+// }
+
+// socket.on("connect", () => {
+//     console.log("Se conecto: ", socket.id);
+// });
 
 
-socket.on("disconnect", () => {
-    console.log("disconnect: ", socket.id)
-    checkSocketStatus();
+// socket.on("disconnect", () => {
+//     console.log("disconnect: ", socket.id)
+//     checkSocketStatus();
+// })
+
+// socket.on("connect_error", () => {
+//     console.log("/")
+// })
+
+// socket.io.on("reconnection_attempt", () => {
+//     console.log("connect try?");
+// })
+
+// socket.io.on("reconnect", () => {
+//     console.log("Reconnected");
+// });
+
+/* -------------------------- RECIBIENDO EVENTOS ---------------------------------------- */
+
+socket.on("Welcome", data => {
+    
+    const text = document.querySelector("#text");
+
+    text.textContent = data;
+    
 })
-
-socket.on("connect_error", () => {
-    console.log("/")
-})
-
-socket.io.on("reconnection_attempt", () => {
-    console.log("connect try?");
-})
-
-socket.io.on("reconnect", () => {
-    console.log("Reconnected");
-});
-
-
-
 
